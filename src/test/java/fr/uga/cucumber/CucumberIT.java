353313c69e599412d27fad406362497ebd300f7f
Thomas Bach <thomasbach.contact@gmail.com>
2022-09-27 11:00:05 +0200

    Initial version of ecom generated by generator-jhipster@7.9.3

---
+++ src/test/java/fr/uga/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package fr.uga.cucumber;
+
+import fr.uga.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
---
+++ src/test/java/fr/uga/cucumber/CucumberTestContextConfiguration.java
@@ -0,0 +1,10 @@
+package fr.uga.cucumber;
+
+import fr.uga.IntegrationTest;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@CucumberContextConfiguration
+@IntegrationTest
+@WebAppConfiguration
+public class CucumberTestContextConfiguration {}
